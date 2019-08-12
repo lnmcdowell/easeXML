7b56a07532a22a6a3f1d3234cf9d4ff4cae54e2e
Larry Nathaniel Mcdowell <nathaniel29651@gmail.com>
2019-08-12 13:36:17 -0400

    Initial Commit

---
+++ easeXML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easeXML
+//
+//  Created by Larry Mcdowell on 8/12/19.
+//  Copyright © 2019 Larry Mcdowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
